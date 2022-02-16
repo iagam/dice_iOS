ba65fcc854b4167ec1c18ee3940484b98ff3bcda
iagam <iamagam03@gmail.com>
2022-02-16 23:34:57 +0530

    Use IBOutlet to reference element

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,17 +2,29 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Agam
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageViewL: UIImageView!
+    @IBOutlet weak var diceImageViewR: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Who         What    Value
+        diceImageViewL.image = UIImage(named: "DiceSix")
+        diceImageViewR.image = UIImage(named: "DiceTwo")
+        
+        // Who         What    Value
+        diceImageViewL.alpha = 0.9
+        diceImageViewR.alpha = 0.9
+        
+        
     }
 
 
