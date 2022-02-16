25cebc8636966ab22f449fc3f640e6ddd4f4fa2f
iagam <iamagam03@gmail.com>
2022-02-17 02:35:37 +0530

    App randomisation completed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,24 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewL: UIImageView!
     @IBOutlet weak var diceImageViewR: UIImageView!
     
+    let diceArray = [UIImage(named: "DiceOne"),
+    UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"),
+    UIImage(named: "DiceFour"), UIImage(named: "DiceFive"),
+    UIImage(named: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Who         What    Value
-        diceImageViewL.image = UIImage(named: "DiceSix")
-        diceImageViewR.image = UIImage(named: "DiceTwo")
-        
-        // Who         What    Value
-        diceImageViewL.alpha = 0.9
-        diceImageViewR.alpha = 0.9
     }
 
-    //IBAction allows UI to change code on press of the button
+    //IBAction allows UI to change value based on press of the button
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        // Who         What    Value
-        diceImageViewL.image = UIImage(named: "DiceFour")
-        diceImageViewR.image = UIImage(named: "DiceFour")
+        diceImageViewL.image = diceArray.randomElement()!
+        diceImageViewR.image = diceArray.randomElement()!
         
     }
     
